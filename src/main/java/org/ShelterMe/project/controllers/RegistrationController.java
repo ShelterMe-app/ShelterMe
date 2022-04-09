bb8e4e484fd75839032be4dc1044e9aa810796d9
Gabor Sebastian <sebi.gabor20@outlook.com>
2022-04-09 22:39:10 +0300

    Created project template

---
+++ src/main/java/org/ShelterMe/project/controllers/RegistrationController.java
@@ -0,0 +1,36 @@
+package org.ShelterMe.project.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.ShelterMe.project.exceptions.UsernameAlreadyExistsException;
+import org.ShelterMe.project.services.UserService;
+
+public class RegistrationController {
+
+    @FXML
+    private Text registrationMessage;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private ChoiceBox role;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Affected", "Volunteer");
+    }
+
+    @FXML
+    public void handleRegisterAction() {
+        try {
+            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
+            registrationMessage.setText("Account created successfully!");
+        } catch (UsernameAlreadyExistsException e) {
+            registrationMessage.setText(e.getMessage());
+        }
+    }
+}
